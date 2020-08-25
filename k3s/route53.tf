0192eaa1fcb4052bdb6f5b6d54f81a4671de9566
David Salek <salekd@gmail.com>
2020-08-25 14:16:46 +0200

    no record for the cluster

--- k3s/route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "apiserver" {
-  zone_id = var.zone_id
-  name    = var.record_name
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.k3s_master.public_ip]
-}
